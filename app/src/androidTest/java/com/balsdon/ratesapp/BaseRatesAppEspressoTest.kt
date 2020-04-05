7c4e8ea7caca46e35be83a0027bcd397fb3f822e
Quintin Balsdon <qbalsdon@gmail.com>
2020-04-05 10:44:56 +0100

    adds all v-23 icons as png's fixes tests for v23 device and resets prefs for each test updates README

--- app/src/androidTest/java/com/balsdon/ratesapp/BaseRatesAppEspressoTest.kt
+++ app/src/androidTest/java/com/balsdon/ratesapp/BaseRatesAppEspressoTest.kt
@@ -1,22 +1,37 @@
 package com.balsdon.ratesapp
 
+import android.content.Context
+import android.content.Intent
+import android.preference.PreferenceManager
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import com.balsdon.ratesapp.mocks.EspressoMockRateFetcher
 import com.balsdon.ratesapp.view.RateListActivity
-import org.junit.Assert
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 open class BaseRatesAppEspressoTest {
     @get:Rule
     var activityRule: ActivityTestRule<RateListActivity> =
-        ActivityTestRule(RateListActivity::class.java)
+        ActivityTestRule(RateListActivity::class.java,true, false)
 
     protected val espressoApplication by lazy {
         (activityRule.activity.application as EspressoApplication)
     }
 
-    //Assert we're testing with the right application
+    @Before
+    fun clearPreferences() {
+        val targetContext: Context = getInstrumentation().targetContext
+        PreferenceManager.getDefaultSharedPreferences(targetContext).edit().apply {
+            clear()
+            apply()
+        }
+
+        activityRule.launchActivity(Intent())
+    }
+
     @Test
     @Throws(Exception::class)
     fun testRunningTheCorrectApplication() {
--- app/src/androidTest/java/com/balsdon/ratesapp/behaviour/RateListActivityEntryBehaviourInstrumentedTest.kt
+++ app/src/androidTest/java/com/balsdon/ratesapp/behaviour/RateListActivityEntryBehaviourInstrumentedTest.kt
@@ -7,10 +7,8 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.balsdon.ratesapp.BaseRatesAppEspressoTest
 import com.balsdon.ratesapp.R
-import com.balsdon.ratesapp.TestRunner.Companion.assertDefaultStatus
 import com.balsdon.ratesapp.TestRunner.Companion.assertHeaderHas
 import com.balsdon.ratesapp.TestRunner.Companion.assertListItemHas
-import com.balsdon.ratesapp.TestRunner.Companion.resetToEuro
 import com.balsdon.ratesapp.view.RateListAdapter
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -19,7 +17,6 @@ import org.junit.runner.RunWith
 class RateListActivityEntryBehaviourInstrumentedTest: BaseRatesAppEspressoTest() {
     @Test
     fun recyclerViewClickOnItemChangesMain() {
-        assertDefaultStatus()
         onView(withId(R.id.currency_list))
             .perform(
                 RecyclerViewActions.actionOnItemAtPosition<RateListAdapter.RateItemViewHolder>(
@@ -30,10 +27,6 @@ class RateListActivityEntryBehaviourInstrumentedTest: BaseRatesAppEspressoTest()
 
         assertHeaderHas("CAD", "Canadian Dollar", "6.00", R.drawable.ic_canada)
         assertListItemHas(4, "CHF", "Swiss Franc", "48.00", R.drawable.ic_switzerland)
-        assertListItemHas(5, "CNY", "Chinese Yuan", "60.00", R.drawable.ic_china)
-        assertListItemHas(6, "CZK", "Czech Koruna", "72.00", R.drawable.ic_czech_republic)
-
-        resetToEuro()
     }
 }
 
